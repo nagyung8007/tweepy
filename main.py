b489d2a8b0e7c56757459618a27c49dc4b395318
NG <90690015+nagyung8007@users.noreply.github.com>
2023-09-08 00:11:43 +0900

    Create main.py
    트위터API 연결

---
+++ main.py
@@ -0,0 +1,19 @@
+#pip install tweepy
+import tweepy
+
+## 트위터 api와 연결
+def connect_api():
+
+    consumer_key = 'consumer_key'
+    consumer_secret = 'consumer_secret'
+    access_token = 'access_token'
+    access_token_secret = 'access_token_secret'
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    return api
+
+print(connect_api())
