9781cc6282288d8159e4620781d15689bf2a2978
NG <90690015+nagyung8007@users.noreply.github.com>
2023-09-08 17:50:03 +0900

    Update main 4.py
    트위터 OAuth2 ID를 발급받은 뒤 수정했지만 인증 오류가 계속 뜹니다. 트위터에서 endpoint 오류가 발생하는 이유는 free 계정에서 tweet

--- main.py
+++ main.py
@@ -1,50 +1,14 @@
-# tweepy >= 4.9
-# twitter API V2
-
-
-# pip install tweepy
 import tweepy
+from google.cloud import language_v1
+import matplotlib.pyplot as plt
+from pytwitter import Api
 
+# OAuth 1.0A
+# api = Api(bearer_token="1679322074089914369-tNyXw8A7nBTyxd7RRlneG2EvG4NBFv")
 
-# Tweepy 인증 설정
-consumer_key = 'consumer_key'
-consumer_secret = 'consumer_secret'
-access_token = 'access_token'
-access_token_secret = 'access_token_secret'
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-
-
-
-# tweepy.StreamClient 클래스를 상속받는 클래스
-class TwitterStream(tweepy.StreamingClient):
-    def on_data(self, raw_data):
-        print(raw_data)
-
-# 규칙 제거 함수
-def delete_all_rules(rules):
-    # 규칙 값이 없는 경우 None 으로 들어온다.
-    if rules is None or rules.data is None:
-        return None
-    stream_rules = rules.data
-    ids = list(map(lambda rule: rule.id, stream_rules))
-    client.delete_rules(ids=ids)
-
-# 스트림 클라이언트 인스터턴스 생성
-client = TwitterStream(access_token)
-
-# 모든 규칙 불러오기 - id값을 지정하지 않으면 모든 규칙을 불러옴
-rules = client.get_rules()
-
-# 모든 규칙 제거
-delete_all_rules(rules)
-
-# 스트림 규칙 추가
-client.add_rules(tweepy.StreamRule(value="#Elon_Musk has:images"))
-
-# 스트림 시작
-client.filter()
-
+# OAuth2.0
+api = Api(client_id="OWk1c2ZnbVFlR0ZUYWZJZ240YjU6MTpjaQ", oauth_flow=True)
+# get the url and code verifier for user to authorize
+url, code_verifier, _ = api.get_oauth2_authorize_url()
+api.generate_oauth2_access_token("https://localhost/?state=state&code=code", code_verifier)
+# copy the response url
